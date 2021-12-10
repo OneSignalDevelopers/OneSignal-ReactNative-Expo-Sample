67b7811865ea357a6c4718ff2c1b6362ed980ad1
Patricio Vargas <pato@onesignal.com>
2021-12-10 13:13:39 -0500

    OneSignal setup for android

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import OneSignal from 'react-native-onesignal';
 
 export default function App() {
+  OneSignal.setAppId("e8e008e8-fadc-457e-ba92-a347e42c109b");
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
